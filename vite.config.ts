bd90dc0a263e5669e5fee1e3bc27715e7171a2f7
Aarif <skaarif.10@gmail.com>
2024-10-03 10:20:42 +0530

    Fixed Router Error Blank Page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+
+
+// Check if we are in development or production mode
+// const isProduction = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
